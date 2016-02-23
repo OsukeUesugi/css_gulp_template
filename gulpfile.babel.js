56332aac7e297f97f784d542226dd3ef4b517bab
Osuke Uesugi <osuke.uesugi@rakuten.com>
2016-02-23 10:40:39 +0900

    Add csslint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,13 +2,16 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import plumber from 'gulp-plumber';
 import autoprefixer from 'gulp-autoprefixer';
+import csslint from 'gulp-csslint';
 
 gulp.task('sass', () => {
 	gulp.src('./sass/**/*.scss')
 	.pipe(plumber())
 	.pipe(sass({outputStyle: 'expanded'}))
 	.pipe(autoprefixer())
-	.pipe(gulp.dest('./css'));
+	.pipe(csslint())
+	.pipe(gulp.dest('./css'))
+	.pipe(csslint.reporter());
 });
 
 gulp.task('watch', () => {
